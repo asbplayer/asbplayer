cfc1b9f43db54d2c7cd5cf2bd605e046e1a060fc
ShanaryS <shanarys@gmail.com>
2026-07-06 10:18:15 -0400

    eslint: no-empty-object-type

--- common/web-socket-client/web-socket-client.ts
+++ common/web-socket-client/web-socket-client.ts
@@ -21,7 +21,7 @@ interface MineSubtitleResponseBody {
     published: boolean;
 }
 
-interface LoadSubtitlesResponseBody {}
+type LoadSubtitlesResponseBody = Record<string, never>;
 
 export interface SubtitleFile {
     base64: string;
@@ -47,7 +47,7 @@ export interface SeekTimestampCommand {
 export interface GetBoundMediaCommand {
     command: 'get-bound-media';
     messageId: string;
-    body: {};
+    body: Record<string, never>;
 }
 
 export interface BoundMedia {
@@ -169,7 +169,7 @@ export class WebSocketClient {
                     } else if (payload.command === 'seek-timestamp') {
                         const messageId = payload.messageId;
                         await this.onSeekTimestamp?.(payload);
-                        const response: Response<{}> = {
+                        const response: Response<Record<string, never>> = {
                             command: 'response',
                             messageId,
                             body: {},
