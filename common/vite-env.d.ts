f6954cac52d52997ec27090600b015b276b47408
R-J Lim <kgerbil@gmail.com>
2026-09-26 19:10:41 +0900

    User-facing app version allows linking to any repo path

--- common/vite-env.d.ts
+++ common/vite-env.d.ts
@@ -2,7 +2,7 @@
 // When bundled by client/extension, this interface merges with their `vite/client` types.
 interface ImportMetaEnv {
     readonly MODE: string;
-    readonly VITE_APP_GIT_COMMIT: string;
+    readonly VITE_APP_VERSION_REPO_PATH: string;
 }
 
 interface ImportMeta {
